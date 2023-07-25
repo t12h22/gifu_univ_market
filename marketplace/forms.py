3ae8814580ca35b2126ca7cf36d20bb91d5a02ca
thori <t12h22.1997@gmail.com>
2023-07-25 20:20:21 +0900

    不要なコメント削除

--- marketplace/forms.py
+++ marketplace/forms.py
@@ -5,5 +5,4 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        # fields = ('name', 'description', 'price', 'category', 'image')
         fields = ('name', 'description', 'price', 'image')
