991117c84800ae383d675bbf6da2bcbac5587763
thori <t12h22.1997@gmail.com>
2023-10-02 18:45:25 +0900

    change repository name

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gifu_univ_market.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market_place.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- market_place/asgi.py
+++ market_place/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for gifu_univ_market project.
+ASGI config for market_place project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gifu_univ_market.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'market_place.settings')
 
 application = get_asgi_application()
